b5b21c36cd3448340f0fbe8008f1b16142e40e09
prabhath-kj <view.prabhath.kj@gmail.com>
2023-10-09 21:08:14 +0530

    function overloading added

--- functionOverloading.ts
+++ functionOverloading.ts
@@ -1,4 +1,25 @@
-function add(num1:number,num2:number):number{
+// one way of function overloading
+
+// function add(num1:number|string,num2:number|string):number|string{
+//    if(typeof num1=="string"|| typeof num2=="string"){
+//     return num1+""+num2;
+//    }else{
+//     return num1+num2;
+//    }
+// }
+
+
+// add(2,2)
+// add("2","2")
+
+
+//second way
+
+function add(num1:number,num2:number):number
+function add(num1:string,num2:string):string
+function add(num1:any,num2:any):any{
     return num1+num2
 }
+
 add(2,2)
+add("2","2")
